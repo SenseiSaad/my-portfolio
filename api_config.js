7dc4aba2cd8bc8c5c604cfa718a96fe0a86a2c3e
Saad Waseem <obsks23@gmail.com>
2026-03-28 00:10:53 +0500

    add AWS EB url

--- api_config.js
+++ api_config.js
@@ -1,5 +1,5 @@
 const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-const API_BASE_URL = isLocal ? 'http://127.0.0.1:8000' : 'https://api.slancer.site';
+const API_BASE_URL = isLocal ? 'http://portfolio-backend-env.eba-7mp6mbh4.us-east-1.elasticbeanstalk.com/' : 'https://api.slancer.site';
 
 // Dynamically route the Admin logins based on environment
 document.addEventListener('DOMContentLoaded', () => {
